e2513a6d4786c702eb6e028ec6296a6bd09ef465
LuanMendesMoura <luanmendesmoura091207@gmail.com>
2025-06-24 21:52:00 -0400

    criando regras para conta

--- src/Controller/UsuariosController.php
+++ src/Controller/UsuariosController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Dto\UsuarioDto;
+use App\Entity\Conta;
 use App\Entity\Usuario;
 use App\Repository\UsuarioRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -72,10 +73,21 @@ final class UsuariosController extends AbstractController
 
         // criar registro na tb usuario
         $entityManager->persist($usuario);
-        $entityManager->flush();
+
+        // instanciar o objeto conta
+        $conta = new Conta();
+        // $numeroConta = preg_replace('/\D/', '', uniqid());
+        $numeroConta = rand(1, 99999);
+        $conta->setNumero($numeroConta);
+        $conta->setSaldo('0');
+        $conta->setUsuario($usuario);
 
         // criar registro na tb conta
+        $entityManager->persist($conta);
+        $entityManager->flush();
+        
         // retornar os dados de usuario e conta
+
 
         return $this->json([$usuario]);
     }
